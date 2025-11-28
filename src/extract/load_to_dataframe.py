c7855f18845102fad3f15b5b0ebb586cb745600f
Aiden Tempest <aidentempest99@gmail.com>
2025-11-28 16:56:32 +0000

    testing done

--- src/extract/load_to_dataframe.py
+++ src/extract/load_to_dataframe.py
@@ -5,5 +5,8 @@ import pandas as pd
 
 def load_to_dataframe(dir_path: str, file: str) -> pd.DataFrame:
     csv_path = os.path.join(dir_path, file)
-    df = pd.read_csv(csv_path)
+    try:
+        df = pd.read_csv(csv_path)
+    except Exception as e:
+        raise Exception(f"Error: {e}")
     return df
