9bbe99690bdc60b75d5570d2530b599c451ec488
Aiden Tempest <aidentempest99@gmail.com>
2025-12-01 15:21:18 +0000

    visualisation: added 01

--- streamlit/pages/general.py
+++ streamlit/pages/general.py
@@ -1,3 +1,20 @@
-import streamlit as st
+import os
+import sys
 
-st.header("hello world")
+import streamlit as st
+import humanize
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(ROOT_DIR)
+
+from src.constants import OUTPUT_PATH
+from src.extract.load_to_dataframe import load_to_dataframe
+
+df_01 = load_to_dataframe(OUTPUT_PATH, "01.csv")
+value_01 = df_01.loc[0, "total_planned_services"]
+value_01 = humanize.intcomma(value_01)
+
+st.title("Domestic Dutch Rail Services for 2024")
+st.header("General Statistics")
+
+st.metric("Total planned services", value_01)
