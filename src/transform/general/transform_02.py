baf727bdf7fe1ed55d0af94422f0c7c0c69ad7cd
Aiden Tempest <aidentempest99@gmail.com>
2025-12-02 14:08:25 +0000

    transform: 02 tests passing

--- src/transform/general/transform_02.py
+++ src/transform/general/transform_02.py
@@ -25,46 +25,61 @@ def transform_02(df: pd.DataFrame) -> pd.DataFrame:
     df_merged_rows.drop(columns=["Service:RDT-ID"], inplace=True)
     transformed_df = df_merged_rows.drop_duplicates()
     transformed_df.reset_index(inplace=True, drop=True)
-    print(transformed_df)
     logger.info("Successfully transformed for 02")
     return transformed_df
 
 
 def keep_start_and_end_stations(df: pd.DataFrame) -> pd.DataFrame:
     logger.info("Removing intermediate stations")
-    start_end_mask = (
-        df["Stop:Arrival time"].isnull() | df["Stop:Departure time"].isnull()
-    )
-    start_end_df = df[start_end_mask]
-    start_end_df.reset_index(drop=True, inplace=True)
-    logger.info("Successfully removed intermediate stations")
-    return start_end_df
+    try:
+        start_end_mask = (
+            df["Stop:Arrival time"].isnull() | df["Stop:Departure time"].isnull()
+        )
+        start_end_df = df[start_end_mask]
+        start_end_df.reset_index(drop=True, inplace=True)
+        logger.info("Successfully removed intermediate stations")
+        return start_end_df
+    except Exception as e:
+        logger.error(f"Remove intermediate stations failed: {e}")
+        raise
 
 
 def create_columns(
     df: pd.DataFrame, old_cols: list[str], new_cols: list[str], cond_col: str
 ) -> pd.DataFrame:
     logger.info(f"Creating new columns: {new_cols}")
-    new_df = df.copy()
-    mask = new_df[cond_col].notnull()
-    for i in range(len(old_cols)):
-        new_df.loc[:, new_cols[i]] = None
-        new_df.loc[mask, new_cols[i]] = new_df.loc[mask, old_cols[i]]
-    logger.info("Successfully created new columns")
-    return new_df
+    try:
+        new_df = df.copy()
+        mask = new_df[cond_col].notnull()
+        for i in range(len(old_cols)):
+            new_df.loc[:, new_cols[i]] = None
+            new_df.loc[mask, new_cols[i]] = new_df.loc[mask, old_cols[i]]
+        logger.info("Successfully created new columns")
+        return new_df
+    except Exception as e:
+        logger.error(f"Create new columns failed: {e}")
+        raise
 
 
 def merge_rows(df: pd.DataFrame) -> pd.DataFrame:
     logger.info("Merging rows")
-    merged_df = df.groupby("Service:RDT-ID", as_index=False).first()
-    logger.info("Successfully merged rows")
-    return merged_df
+    try:
+        merged_df = df.groupby("Service:RDT-ID", as_index=False).first()
+        logger.info("Successfully merged rows")
+        return merged_df
+    except Exception as e:
+        logger.error(f"Merge rows failed: {e}")
+        raise
 
 
 def count_services(df: pd.DataFrame):
     logger.info("Counting services")
-    df["route_count"] = df.groupby(
-        ["Service:Type", "Service:Company", "start_station", "end_station"]
-    ).transform("size")
-    df["route_count"] = df["route_count"].fillna(1).astype("int")
-    logger.info("Successfully counted services")
+    try:
+        df["route_count"] = df.groupby(
+            ["Service:Type", "Service:Company", "start_station", "end_station"]
+        ).transform("size")
+        df["route_count"] = df["route_count"].fillna(1).astype("int")
+        logger.info("Successfully counted services")
+    except Exception as e:
+        logger.error(f"Count services failed: {e}")
+        raise
