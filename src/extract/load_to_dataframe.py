35260c6d48312a7f40a9f651a1c87368c676efd7
Aiden Tempest <aidentempest99@gmail.com>
2025-12-04 20:08:55 +0000

    docs: added docstrings to load_to_dataframe.py

--- src/extract/load_to_dataframe.py
+++ src/extract/load_to_dataframe.py
@@ -1,3 +1,5 @@
+"""Loading a CSV into a DataFrame for later usage in transform."""
+
 import os
 
 import pandas as pd
@@ -8,6 +10,15 @@ logger = setup_logger(__name__, "extract.log")
 
 
 def load_to_dataframe(dir_path: str, file: str) -> pd.DataFrame:
+    """Loads a CSV file into a DataFrame.
+
+    Args:
+        dir_path: String representing the directory path of the CSV file.
+        file: String representing the CSV file.
+
+    Returns:
+        The DataFrame the CSV has been loaded into.
+    """
     logger.info(f"Loading {file} into DataFrame")
     csv_path = os.path.join(dir_path, file)
     try:
