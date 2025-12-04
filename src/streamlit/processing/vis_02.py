380bee5e7a2d4e7b26a5f580276e7bb07a98c634
Aiden Tempest <aidentempest99@gmail.com>
2025-12-04 20:47:09 +0000

    docs: added docstrings to vis_02.py

--- src/streamlit/processing/vis_02.py
+++ src/streamlit/processing/vis_02.py
@@ -1,3 +1,5 @@
+"""Background processing for visualisation 05."""
+
 import folium
 import pandas as pd
 
@@ -7,12 +9,26 @@ from src.streamlit.processing.constants import MAP_CENTER, RED
 
 
 def get_data_02() -> pd.DataFrame:
+    """Loads the data needed for visualisation 02 into a DataFrame.
+
+    Returns:
+        DataFrame with the needed data.
+    """
     df_02 = load_to_dataframe(OUTPUT_PATH, "02.csv")
     df_02.sort_values("route_count", ascending=False, inplace=True)
     return df_02
 
 
 def get_map(df: pd.DataFrame) -> folium.Map:
+    """Creates a map with start points, end points, and lines connecting the
+    two for showing the most and least frequent routes.
+
+    Args:
+        df: DataFrame with the needed data.
+
+    Returns:
+        A Folium map with the datapoints plotted.
+    """
     map = folium.Map(location=MAP_CENTER, zoom_start=7)
     for _, row in df.iterrows():
         start_point = [row["start_lat"], row["start_lng"]]
